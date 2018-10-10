f325a8c01ea24ee965ed453cc022a43886b87cbf
Kyle Baley <kyle@baley.org>
2018-10-10 12:09:19 -0500

    Calculate running total client side on add/delete

--- Coronado.Web/ClientApp/src/store/TransactionList.js
+++ Coronado.Web/ClientApp/src/store/TransactionList.js
@@ -1,33 +1,34 @@
-﻿import { filter, orderBy, concat } from 'lodash'; 
-import { createSelector } from 'reselect';
+﻿import { filter, orderBy, concat, forEachRight } from 'lodash'; 
 const deleteTransactionType = 'DELETE_TRANSACTION';
 const setTransactionListType = 'SET_TRANSACTION';
 const receiveNewTransactionType = 'RECEIVE_TRANSACTION';
 
 const initialState = { };
-const getTransactions = state => state.transactionList;
 
-export const getTransactionsWithRunningTotal = createSelector(
-  [getTransactions],
-  (transactions) => {
-    console.log(transactionId);
-    return orderBy(transactions, ['transactionDate'], ['desc'])
-  }
-)
+function computeRunningTotal(transactions) {
+  var total = 0;
+  console.log(transactions);
+  var sorted = orderBy(transactions, ['transactionDate'], ['desc']);
+  forEachRight(sorted, (value) => {
+    total += value.amount;
+    value.runningTotal = total; 
+  });
+  return sorted;
+}
 
 export const actionCreators = {
   setTransactionList: (transactions) => async (dispatch, getState) => {
     dispatch( {type: setTransactionListType, transactions});
   },
   deleteTransaction: (transactionId) => async (dispatch, getState) => {
-    // const response = await fetch('/api/Transactions/' + transactionId, {
-    //   method: 'DELETE',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json'
-    //   }
-    // });
-    // const transaction = await response.json();
+    const response = await fetch('/api/Transactions/' + transactionId, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    const transaction = await response.json();
     dispatch( { type: deleteTransactionType, transactionId } );
   },
 
@@ -59,14 +60,14 @@ export const reducer = (state, action) => {
   if (action.type === deleteTransactionType) {
     return {
       ...state,
-      transactionList: filter(state.transactions, (n) => { return n.transactionId !== action.transactionId })
+      transactionList: computeRunningTotal(filter(state.transactionList, (n) => { return n.transactionId !== action.transactionId }))
     }
   }
 
   if (action.type === receiveNewTransactionType) {
     return {
       ...state,
-      transactionList: orderBy(concat(state.transactionList, action.newTransaction), ['transactionDate'], ['desc'])
+      transactionList: computeRunningTotal(concat(state.transactionList, action.newTransaction))
     }
   }
 
