9a3155c3dcc591510df20d3dc677defafba238bd
Kyle Baley <kyle@baley.org>
2018-11-08 23:25:20 -0500

    Add .eslintrc so VS Code shows lint errors

--- Coronado.Web/ClientApp/src/App.test.js
+++ Coronado.Web/ClientApp/src/App.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Layout from './components/Layout';
-import EditIcon from './components/icons/EditIcon';
-
 it('renders without crashing', () => {
-  // EditIcon(props)
   expect(true).toBe(true);
 });
