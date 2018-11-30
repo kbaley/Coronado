6bccc8ed6fba20fd6142fa008a4048708e756e87
Kyle Baley <kyle@baley.org>
2018-11-30 18:20:32 -0500

    Number formatting

--- Coronado.Web/ClientApp/src/components/common/DecimalFormat.js
+++ Coronado.Web/ClientApp/src/components/common/DecimalFormat.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './DecimalFormat.css';
 
+function format(number) {
+  return number.toLocaleString('en', {minimumFractionDigits: 2});
+}
+
 export function DecimalFormat(props) {
   var amount = props.amount;
   if (amount <= 0) {
     amount = 0 - amount;
   }
   return (<span className="decimalAmount">
-    {(props.isDebit && props.amount > 0) ? Number(amount).toFixed(2) : ""}
-    {(props.isCredit && props.amount > 0) ? Number(amount).toFixed(2) : ""}
+    {(props.isDebit && props.amount > 0) ? format(amount) : ""}
+    {(props.isCredit && props.amount > 0) ? format(amount) : ""}
   </span>);
 }
 
-export function MoneyFormat(props) {
-  return (<span className="decimalAmount">{Number(props.amount).toFixed(2)}</span>);
+export function MoneyFormat({amount}) {
+  return (<span className="decimalAmount">{format(amount)}</span>);
 }
