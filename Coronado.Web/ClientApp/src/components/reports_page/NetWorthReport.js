ef8d9766b19a891a466955019391def37da74b61
Kyle Baley <kyle@baley.org>
2018-12-09 20:32:46 -0500

    Change change from previous month in net worth report

--- Coronado.Web/ClientApp/src/components/reports_page/NetWorthReport.js
+++ Coronado.Web/ClientApp/src/components/reports_page/NetWorthReport.js
@@ -16,6 +16,13 @@ class NetWorthReport extends Component {
       <div style={{margin: "10px"}}>
         <h4>Net Worth</h4>
         <table className="table" style={{width: "400px"}}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th style={{textAlign: "right"}}>Net worth</th>
+              <th style={{textAlign: "right"}}>Change</th>
+            </tr>
+          </thead>
           <tbody>
             {this.props.report.map( (r, index) =>
               <tr key={index}>
@@ -24,6 +31,11 @@ class NetWorthReport extends Component {
                 </td>
                 <td>
                   <CurrencyFormat value={r.netWorth} />
+                </td>
+                <td>
+                  {index < this.props.report.length - 1 &&
+                  <span><CurrencyFormat value={r.netWorth - this.props.report[index + 1].netWorth} /></span>
+                  }
                 </td>
               </tr>
             )}
