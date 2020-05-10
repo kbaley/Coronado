6a856ce81f5dd401822f0483ad1ae28210b28066
Kyle Baley <kyle@baley.org>
2020-05-10 15:37:46 -0500

    Make new icon green

--- Coronado.Web/ClientApp/src/components/icons/NewIcon.js
+++ Coronado.Web/ClientApp/src/components/icons/NewIcon.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import { IconButton } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
 import { PropTypes } from 'prop-types';
 
+const styles = theme => ({
+  icon: {
+    color: theme.palette.green,
+  }
+})
+
+const useStyles = makeStyles(styles);
+
 export function NewIcon({onClick, fontSize, ...rest}) {
+  const classes = useStyles();
   return (
     <IconButton onClick={onClick} component="span" {...rest}>
       <AddCircleIcon 
+        className={classes.icon}
         fontSize={fontSize ?? "default"}
       />
     </IconButton>
