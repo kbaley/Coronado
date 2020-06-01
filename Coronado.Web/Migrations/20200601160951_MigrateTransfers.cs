a00ae30ef81ca9cb8fdc6d231b685f6ce0c9de61
Kyle Baley <kyle@baley.org>
2020-06-01 12:28:12 -0500

    Add migrations for transfers and related transaction ID

---
+++ Coronado.Web/Migrations/20200601160951_MigrateTransfers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Coronado.Web.Migrations
+{
+    public partial class MigrateTransfers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO transfers (transfer_id, left_transaction_id, right_transaction_id)
+                SELECT uuid_generate_v4(), transaction_id, related_transaction_id
+                FROM transactions
+                WHERE related_transaction_id IS NOT NULL;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM transfers");
+        }
+    }
+}
