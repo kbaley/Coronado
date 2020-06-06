5a8102bf74da2bcdf7e29352ae1e6621985e1570
Kyle Baley <kyle@baley.org>
2020-06-06 07:20:34 -0500

    Add migration to remove unused investment columns

---
+++ Coronado.Web/Migrations/20200606121857_RemoveUnusedInvestmentColumns.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Coronado.Web.Migrations
+{
+    public partial class RemoveUnusedInvestmentColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "average_price",
+                table: "investments");
+
+            migrationBuilder.DropColumn(
+                name: "last_retrieved",
+                table: "investments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "average_price",
+                table: "investments",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_retrieved",
+                table: "investments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
