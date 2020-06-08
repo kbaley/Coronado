da28f854efc9487b3ad7f5d95fac6cb458b1862f
Kyle Baley <kyle@baley.org>
2020-06-08 06:36:44 -0500

    Convert NewCategory to functional component

--- Coronado.Web/ClientApp/src/components/categories/NewCategory.js
+++ Coronado.Web/ClientApp/src/components/categories/NewCategory.js
@@ -1,66 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as categoryActions from '../../actions/categoryActions';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as Mousetrap from 'mousetrap';
 import CategoryForm from './CategoryForm';
 import { NewIcon } from '../icons/NewIcon';
 import './NewCategory.css';
 
-class NewCategory extends Component {
-  displayName = NewCategory.name;
-  constructor(props) {
-    super(props);
-    this.saveCategory = this.saveCategory.bind(this);   
-    this.showForm = this.showForm.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.state = { show: false, 
-        category: {name: '', type: ''}
-    };
-  }
+export default function NewCategory() {
+  const [show, setShow] = React.useState(false);
+  const dispatch = useDispatch();
+  const categories = useSelector(state => state.categories);
 
-  componentDidMount() {
-      Mousetrap.bind('n c', this.showForm);
-  }
+  React.useEffect(() => {
+    Mousetrap.bind('n c', showForm);
 
-  componentWillUnmount() {
+    return function cleanup() {
       Mousetrap.unbind('n c');
-  }
+    }
+  })
 
-  showForm() {
-    this.setState({show:true});
+  const showForm = () => {
+    setShow(true);
     return false;
   }
 
-  saveCategory(category) {
-    this.props.actions.createCategory(category);
+  const saveCategory = (category) => {
+    dispatch(categoryActions.createCategory(category));
   }
 
-  handleClose() {
-    this.setState({show:false});
+  const handleClose = () => {
+    setShow(false);
   }
-  render() {
     return (<span>
-        <NewIcon onClick={this.showForm} className="new-category"/>
-        <CategoryForm show={this.state.show} onClose={this.handleClose} onSave={this.saveCategory}
-          categories={this.props.categories} />
+        <NewIcon onClick={showForm} className="new-category"/>
+        <CategoryForm show={show} onClose={handleClose} onSave={saveCategory}
+          categories={categories} />
       </span>);
-  };
 }
-
-function mapStateToProps(state) {
-  return {
-    categories: state.categories
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(categoryActions, dispatch)
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NewCategory);
