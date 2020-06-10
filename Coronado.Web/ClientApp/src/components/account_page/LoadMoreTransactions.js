c4d7868e3ac00ffe8b5b0105c2728a5d48bb8064
Kyle Baley <kyle@baley.org>
2020-06-10 17:01:59 -0500

    Convert LoadMoreTransactions to functional component

--- Coronado.Web/ClientApp/src/components/account_page/LoadMoreTransactions.js
+++ Coronado.Web/ClientApp/src/components/account_page/LoadMoreTransactions.js
@@ -1,46 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@material-ui/core';
-import { bindActionCreators } from 'redux';
 import * as transactionActions from '../../actions/transactionActions';
 
-class LoadMoreTransactions extends Component {
-  displayName = LoadMoreTransactions.name;
+export default function LoadMoreTransactions() {
+  const remainingTransactionCount = useSelector(state => state.transactionModel.remainingTransactionCount);
+  const selectedAccount = useSelector(state => state.selectedAccount);
+  const dispatch = useDispatch();
 
-  constructor(props) {
-    super(props);
-    this.loadMore = this.loadMore.bind(this);
-    this.state = {
-    };
+  const loadMore = () => {
+    dispatch(transactionActions.loadAllTransactions(selectedAccount));
   }
 
-  loadMore() {
-    this.props.actions.loadAllTransactions(this.props.selectedAccount);
-  }
-
-  render() {
     return (
       <div style={{"margin": "15px 15px 40px 15px"}}>
-        <Button onClick={this.loadMore}>Load {this.props.remainingTransactionCount} more transactions</Button>
+        <Button onClick={loadMore}>Load {remainingTransactionCount} more transactions</Button>
       </div>
     );
-  }
 }
-
-function mapStateToProps(state) {
-  return {
-    remainingTransactionCount: state.transactionModel.remainingTransactionCount,
-    selectedAccount: state.selectedAccount
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-   return {
-     actions: bindActionCreators({...transactionActions}, dispatch)
-   }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(LoadMoreTransactions);
