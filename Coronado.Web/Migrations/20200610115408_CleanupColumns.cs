747ce10d9e942afd965ec331a85b6601cd986519
Kyle Baley <kyle@baley.org>
2020-06-10 08:50:58 -0500

    Clean up some columns in the DB

---
+++ Coronado.Web/Migrations/20200610115408_CleanupColumns.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Coronado.Web.Migrations
+{
+    public partial class CleanupColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "shares",
+                table: "investments");
+
+            migrationBuilder.DropColumn(
+                name: "current_balance",
+                table: "accounts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "shares",
+                table: "investments",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "current_balance",
+                table: "accounts",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
