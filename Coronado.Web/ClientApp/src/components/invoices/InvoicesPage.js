4e76646e3ab89316f5a1b7fb30bb84375169ff46
Kyle Baley <kyle@baley.org>
2020-06-12 15:24:03 -0500

    Convert InvoicesPage to functional component

--- Coronado.Web/ClientApp/src/components/invoices/InvoicesPage.js
+++ Coronado.Web/ClientApp/src/components/invoices/InvoicesPage.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as actions from '../../actions/invoiceActions';
-import { bindActionCreators } from 'redux';
 import NewInvoice from './NewInvoice';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import InvoiceList from "./InvoiceList";
 import UploadInvoiceTemplate from "./UploadInvoiceTemplate";
 import {Icon} from "../icons/Icon";
@@ -11,75 +10,54 @@ import './InvoicesPage.css';
 import PublishIcon from '@material-ui/icons/Publish';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 
-class InvoicesPage extends Component {
-  constructor(props) {
-    super(props);
-    this.showTemplate = this.showTemplate.bind(this);
-    this.uploadTemplate = this.uploadTemplate.bind(this);
-    this.uploadTemplateForm = this.uploadTemplateForm.bind(this);
-    this.handleChangeButton = this.handleChangeButton.bind(this);
-    this.state = {
-      showUploadForm: false,
-      showPaid: false,
-    }
-  }
+export default function InvoicesPage() {
+  const [showUploadForm, setShowUploadForm] = React.useState(false);
+  const [ showPaid, setShowPaid ] = React.useState(false);
+  const dispatch = useDispatch();
 
-  showTemplate() {
+  const showTemplate = () => {
     window.open("/invoice/GenerateHTML");
   }
 
-  uploadTemplateForm() {
-    this.setState({showUploadForm: true});
+  const uploadTemplateForm = () => {
+    setShowUploadForm(true);
   }
 
-  uploadTemplate(file) {
-    this.props.actions.uploadTemplate(file);
+  const uploadTemplate = (file) => {
+    dispatch(actions.uploadTemplate(file));
   }
 
-  handleChangeButton(value) {
-    this.setState({showPaid: value});
+  const handleChangeButton = (value) => {
+    setShowPaid(value);
   }
 
-  render() {
     return (
       <div>
         <div style={{float: "right", width: "350px", textAlign: "right", "marginRight": "30px"}}>
           <ToggleButton
             value="check"
-            selected={this.state.showPaid}
+            selected={showPaid}
             onChange={() => {
-              this.handleChangeButton(!this.state.showPaid);
+              handleChangeButton(!showPaid);
             }}
           >
             Show paid
           </ToggleButton>
           <Icon 
-            onClick={this.uploadTemplateForm}
+            onClick={uploadTemplateForm}
             icon={<PublishIcon />}
           />
           <Icon 
-            onClick={this.showTemplate}
+            onClick={showTemplate}
             icon={<OpenInNewIcon />}
           />
-          <UploadInvoiceTemplate show={this.state.showUploadForm} onHide={() => this.setState({showUploadForm: false})}
-            onUpload={this.uploadTemplate}/>
+          <UploadInvoiceTemplate show={showUploadForm} onHide={() => setShowUploadForm(false)}
+            onUpload={uploadTemplate}/>
         </div>
         <h1>
           Invoices <NewInvoice />
         </h1>
-        <InvoiceList showPaid={this.state.showPaid} />
+        <InvoiceList showPaid={showPaid} />
       </div>
     );
-  }
 }
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch)
-  }
-}
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(InvoicesPage);
