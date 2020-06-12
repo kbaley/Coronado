28cb7c9bf5c06f8e617282c33c2fcabb9478d638
Kyle Baley <kyle@baley.org>
2020-06-12 15:45:44 -0500

    Convert UploadInvoiceTemplate to functional component

--- Coronado.Web/ClientApp/src/components/invoices/UploadInvoiceTemplate.js
+++ Coronado.Web/ClientApp/src/components/invoices/UploadInvoiceTemplate.js
@@ -1,37 +1,24 @@
-import React, { Component } from 'react';
-import * as actions from '../../actions/invoiceActions';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Button, Dialog, DialogTitle, DialogActions, DialogContent } from '@material-ui/core';
 
-export class NewInvoice extends Component {
-  constructor(props) {
-    super(props);
-    this.uploadTemplate = this.uploadTemplate.bind(this);
-    this.handleSelectedFile = this.handleSelectedFile.bind(this);
-    this.state = {  
-      selectedFile: null,
-      filename: 'No file selected'
-    };
+export default function NewInvoice({onUpload, onHide, show}) {
+  const [ selectedFile, setSelectedFile ] = React.useState(null);
+  const [ filename, setFilename ] = React.useState("No file selected");
+
+  const handleSelectedFile = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setFilename(event.target.files[0].name);
   }
 
-  handleSelectedFile(event) {
-    this.setState({
-      selectedFile: event.target.files[0],
-      filename: event.target.files[0].name
-    })
+  const uploadTemplate = () => {
+    onUpload(selectedFile);
   }
 
-  uploadTemplate() {
-    this.props.onUpload(this.state.selectedFile);
-  }
-
-  render() {
     return (
       <span>
         <Dialog
-          onClose={this.props.onHide}
-          open={this.props.show}
+          onClose={onHide}
+          open={show}
           fullWidth={true}
           maxWidth={'sm'}
         >
@@ -41,31 +28,19 @@ export class NewInvoice extends Component {
               type="file" 
               name="selectedFile" 
               id='selectedFile'
-              onChange={this.handleSelectedFile}
+              onChange={handleSelectedFile}
               style={{ display: 'none' }}
             />
             <label htmlFor='selectedFile'>
               <Button variant="outlined" component="span">Select file</Button>
             </label>
             <label style={{marginLeft: 20}}>
-              {this.state.filename}
+              {filename}
             </label>
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.uploadTemplate}>Upload</Button>
+            <Button onClick={uploadTemplate}>Upload</Button>
           </DialogActions>
         </Dialog>
       </span>);
-  };
 }
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch)
-  }
-}
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(NewInvoice);
