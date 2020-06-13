372053c5e158a64889577946d3a2f8d4bd0eb9a9
Kyle Baley <kyle@baley.org>
2020-06-13 18:46:50 -0500

    After editing or adding categories, show in proper order

--- Coronado.Web/ClientApp/src/reducers/categoryReducer.js
+++ Coronado.Web/ClientApp/src/reducers/categoryReducer.js
@@ -1,33 +1,36 @@
 import initialState from './initialState';
 import * as actions from "../constants/categoryActionTypes.js";
-import { cloneDeep, find } from 'lodash';
+import { cloneDeep, find, orderBy } from 'lodash';
 
 export const categoryReducer = (state = initialState.categories, action, deletedCategories) => {
   switch (action.type) {
     case actions.LOAD_CATEGORIES_SUCCESS:
-      return action.categories;
+      return orderBy(action.categories, ['name'], ['asc']);
       
     case actions.DELETE_CATEGORY:
       return cloneDeep(state.filter(c => c.categoryId !== action.categoryId));
       
     case actions.UNDO_DELETE_CATEGORY:
       const deletedCategory = find(deletedCategories, c => c.categoryId === action.categoryId);
-      return [
+      const newState = [
         ...state,
         Object.assign({}, deletedCategory)
       ];
+      return orderBy(newState, ['name'], ['asc']);
       
     case actions.CREATE_CATEGORY_SUCCESS:
-      return [
+      var newState = [
         ...state,
         Object.assign({}, action.category),
       ];
+      return orderBy(newState, ['name'], ['asc']);
       
     case actions.UPDATE_CATEGORY_SUCCESS:
-      return [
+      var newState = [
         ...state.filter(c => c.categoryId !== action.category.categoryId),
         Object.assign({}, action.category)
       ];
+      return orderBy(newState, ['name'], ['asc']);
       
     default:
       return state;
