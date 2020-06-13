89370e52e2df2edbfaabdead410a824287a17ce8
Kyle Baley <kyle@baley.org>
2020-06-13 10:14:02 -0500

    Convert AccountNavList to functional component

--- Coronado.Web/ClientApp/src/components/Sidebar/AccountNavList.js
+++ Coronado.Web/ClientApp/src/components/Sidebar/AccountNavList.js
@@ -1,108 +1,69 @@
-import React, { Component } from 'react';
-import * as accountActions from '../../actions/accountActions';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import * as Mousetrap from 'mousetrap';
-import { withRouter } from 'react-router-dom';
 import { MoneyFormat } from '../common/DecimalFormat';
 import Spinner from '../common/Spinner';
 import { filter } from 'lodash';
 import { SidebarMenuItem } from './SidebarMenuItem';
 import NewAccount from '../account_page/NewAccount';
 import ToggleAllAccounts from './ToggleAllAccounts';
-import { Toolbar, withStyles, List } from '@material-ui/core';
+import { Toolbar, List } from '@material-ui/core';
 import getIcon from './AccountNavListIcons';
+import history from '../../history';
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
+const styles = ({
 
 });
 
-class AccountNavList extends Component {
-  displayName = AccountNavList.name;
+const useStyles = makeStyles(styles);
 
-  constructor(props) {
-    super(props);
-    this.goToAccount = this.goToAccount.bind(this);
-    this.getBalance = this.getBalance.bind(this);
-    this.state = { 
-      isLoading: true, 
-      accounts: props.accounts,
-    };
-  }
+export default function AccountNavList() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const accounts = useSelector(state => state.showAllAccounts ? state.accounts : filter(state.accounts, a => !a.isHidden));
+  const isLoadingData = useSelector(state => state.loading ? state.loading.accounts : true);
 
-  componentDidUpdate() {
+  React.useEffect(() => {
 
-    if (this.state.isLoading && this.props.accounts && this.props.accounts.length > 0) {
-      for (var i = 0; i < this.props.accounts.length; i++) {
+    function goToAccount(e) {
+      var key = parseInt(e.key, 10) - 1;
+      history.push('/account/' + accounts[key].accountId);
+    }
+
+    if (isLoading && accounts && accounts.length > 0) {
+      for (var i = 0; i < accounts.length; i++) {
         if (i < 10) {
-          Mousetrap.bind('g ' + (i + 1), this.goToAccount);
+          Mousetrap.bind('g ' + (i+1), goToAccount);
         }
       }
-      this.setState({
-        isLoading: false,
-        accounts: this.props.accounts,
-      });
+      setIsLoading(false);
     }
-  }
 
-  componentWillUnmount() {
-    for (var i = 0; i < 10; i++) {
-      Mousetrap.unbind('g ' + (i + 1));
-    }
-  }
+    // No cleanup function here to unbind the mousetrap bindings
+    // because it runs on each render change, so it will "cleanup"
+    // the bindings but due to the logic of setting the bindings
+    // they will never get reinstated. This is okay because the
+    // navlist is always visible
+  }, [isLoading, accounts]);
 
-  goToAccount(e) {
-    var key = parseInt(e.key, 10) - 1;
-    this.props.history.push('/account/' + this.props.accounts[key].accountId);
-  }
-
-  getBalance(account) {
-    return account.currentBalance;
-  }
-
-  render() {
-    const { classes } = this.props;
-    const { history } = this.props;
-    const pathname = history.location.pathname;
-    return (
-      <List className={classes.list}>
-        <Toolbar disableGutters={true} className={classes.toolbar} >
-          <NewAccount />
-          <ToggleAllAccounts />
-        </Toolbar>
-        {this.props.isLoadingData ? <Spinner /> :
-          this.props.accounts.map((account, index) => (
-              <SidebarMenuItem
-              key={index}
-              to={'/account/' + account.accountId}
-              selected={'/account/' + account.accountId === pathname}
-              primary={account.name}
-              secondary={<MoneyFormat amount={account.currentBalance} />}
-              icon={getIcon(account.accountType)} />
-          ))}
-      </List>
-    );
-  }
+  const pathname = history.location.pathname;
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <Toolbar disableGutters={true} className={classes.toolbar} >
+        <NewAccount />
+        <ToggleAllAccounts />
+      </Toolbar>
+      {isLoadingData ? <Spinner /> :
+        accounts.map((account, index) => (
+          <SidebarMenuItem
+            key={index}
+            to={'/account/' + account.accountId}
+            selected={'/account/' + account.accountId === pathname}
+            primary={account.name}
+            secondary={<MoneyFormat amount={account.currentBalance} />}
+            icon={getIcon(account.accountType)} />
+        ))}
+    </List>
+  );
 }
-
-function mapStateToProps(state) {
-  return {
-    accounts: state.showAllAccounts ? state.accounts : filter(state.accounts, a => !a.isHidden),
-    isLoadingData: state.loading ? state.loading.accounts : true,
-    showAllAccounts: state.showAllAccounts,
-    currencies: state.currencies,
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(accountActions, dispatch)
-  }
-}
-
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps,
-  null,
-  { pure: false }
-)(withStyles(styles)(AccountNavList)));
