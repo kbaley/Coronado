6e3d095089d0fc693c3571b0bcb54acc8553095e
Kyle Baley <kyle@baley.org>
2020-06-13 09:19:23 -0500

    Convert CategoryList to functional component

--- Coronado.Web/ClientApp/src/components/categories/CategoryList.js
+++ Coronado.Web/ClientApp/src/components/categories/CategoryList.js
@@ -1,104 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as categoryActions from '../../actions/categoryActions';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CategoryForm from './CategoryForm';
 import { find } from 'lodash';
 import { CategoryRow } from './CategoryRow';
 import Spinner from '../common/Spinner';
-import { 
-  Table, 
-  TableHead, 
+import {
+  Table,
+  TableHead,
   TableBody,
-  TableRow, 
-  TableCell 
+  TableRow,
+  TableCell
 } from '@material-ui/core';
 
-class CategoryList extends Component {
-  constructor(props) {
-    super(props);
-    this.deleteCategory = this.deleteCategory.bind(this);
-    this.startEditing = this.startEditing.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.saveCategory = this.saveCategory.bind(this);
-    this.getCategoryName = this.getCategoryName.bind(this);
-    this.state = {
-      show: false,
-      selectedCategory: {}
-    }
+export default function CategoryList() {
+  const [show, setShow] = React.useState(false);
+  const [selectedCategory, setSelectedCategory] = React.useState({});
+  const categories = useSelector(state => state.categories);
+  const isLoading = useSelector(state => state.loading.categories);
+  const dispatch = useDispatch();
+
+  const deleteCategory = (categoryId, categoryName) => {
+    dispatch(categoryActions.deleteCategory(categoryId, categoryName));
   }
 
-  deleteCategory(categoryId, categoryName) {
-    this.props.actions.deleteCategory(categoryId, categoryName);
+  const startEditing = (category) => {
+    setSelectedCategory(category);
+    setShow(true);
   }
 
-  startEditing(category) {
-    this.setState({show:true, selectedCategory: category});
+  const handleClose = () => {
+    setShow(false);
+    setSelectedCategory({});
   }
 
-  handleClose() {
-    this.setState({show:false});
+  const saveCategory = (category) => {
+    dispatch(categoryActions.updateCategory(category));
   }
 
-  saveCategory(category) {
-    this.props.actions.updateCategory(category);
-  }
-
-  getCategoryName(categoryId) {
+  const getCategoryName = (categoryId) => {
     if (!categoryId || categoryId === '') return '';
 
-    var category = find(this.props.categories, c => c.categoryId === categoryId);
+    var category = find(categories, c => c.categoryId === categoryId);
     return category ? category.name : '';
   }
-  
-  render() {
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell style={{width: 130}}></TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Type</TableCell>
-            <TableCell>Parent</TableCell>
-          </TableRow>
-        </TableHead>
-        <CategoryForm 
-          show={this.state.show} 
-          onClose={this.handleClose} 
-          category={this.state.selectedCategory} 
-          categories={this.props.categories}
-          onSave={this.saveCategory} />
-        <TableBody>
-        { this.props.isLoading ? <tr><td colSpan="5"><Spinner /></td></tr> :
-          this.props.categories.map(cat => 
-        <CategoryRow 
-          key={cat.categoryId} 
-          parent={this.getCategoryName(cat.parentCategoryId)}
-          category={cat} 
-          onEdit={() => this.startEditing(cat)} 
-          onDelete={()=>this.deleteCategory(cat.categoryId, cat.name)} />
-        )}
-        </TableBody>
-      </Table>
-    );
-  }
-}
 
-function mapStateToProps(state) {
-  return {
-    categories: state.categories,
-    notifications: state.notifications,
-    isLoading: state.loading.categories
-  }
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell style={{ width: 130 }}></TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Type</TableCell>
+          <TableCell>Parent</TableCell>
+        </TableRow>
+      </TableHead>
+      <CategoryForm
+        show={show}
+        onClose={handleClose}
+        category={selectedCategory}
+        categories={categories}
+        onSave={saveCategory} />
+      <TableBody>
+        {isLoading ? <tr><td colSpan="5"><Spinner /></td></tr> :
+          categories.map(cat =>
+            <CategoryRow
+              key={cat.categoryId}
+              parent={getCategoryName(cat.parentCategoryId)}
+              category={cat}
+              onEdit={() => startEditing(cat)}
+              onDelete={() => deleteCategory(cat.categoryId, cat.name)} />
+          )}
+      </TableBody>
+    </Table>
+  );
 }
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(categoryActions, dispatch)
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(CategoryList);
