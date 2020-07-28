28dcfcffec2d8cde2ce49fe63177a9b25dedd097
Kyle Baley <kyle@baley.org>
2020-07-28 15:36:40 -0600

    Display investment transactions better on mobile

--- Coronado.Web/ClientApp/src/components/investments_page/InvestmentTransactionList.js
+++ Coronado.Web/ClientApp/src/components/investments_page/InvestmentTransactionList.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Hidden, Divider } from '@material-ui/core';
 import { MoneyFormat } from '../common/DecimalFormat';
 import DateFormat from '../common/DateFormat';
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  accountName: {
+    color: theme.palette.gray[6],
+    padding: 0,
+    borderBottom: "1px solid",
+  }
+})
 
 function Header({ children }) {
   return (
@@ -10,27 +19,40 @@ function Header({ children }) {
     >{children}</Typography>
   );
 }
+
+const useStyles = makeStyles(styles);
+
 export default function InvestmentTransactionList({ investment }) {
+  const classes = useStyles();
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography variant="h4">Transactions</Typography>
       </Grid>
-      <Grid item xs={2}><Header>Date</Header></Grid>
-      <Grid item xs={4}><Header>Account</Header></Grid>
-      <Grid item xs={2}><Header>Shares</Header></Grid>
-      <Grid item xs={2}><Header>Price</Header></Grid>
-      <Grid item xs={2}><Header>Total</Header></Grid>
+      <Grid item xs={3} md={2}><Header>Date</Header></Grid>
+      <Hidden smDown>
+        <Grid item md={4}><Header>Account</Header></Grid>
+      </Hidden>
+      <Grid item xs={3} md={2}><Header>Shares</Header></Grid>
+      <Grid item xs={3} md={2}><Header>Price</Header></Grid>
+      <Grid item xs={3} md={2}><Header>Total</Header></Grid>
       {investment.transactions && investment.transactions.map((t, index) =>
         <React.Fragment key={t.investmentTransactionId}>
-          <Grid item xs={2}>{DateFormat(t.date)}</Grid>
-          <Grid item xs={4}>{t.sourceAccountName}</Grid>
-          <Grid item xs={2}>{t.shares}</Grid>
-          <Grid item xs={2}>{t.price.toFixed(4)}</Grid>
-          <Grid item xs={2}>
+          <Grid item xs={3} md={2}>{DateFormat(t.date)}</Grid>
+          <Hidden smDown>
+            <Grid item md={4}>{t.sourceAccountName}</Grid>
+          </Hidden>
+          <Grid item xs={3} md={2}>{t.shares}</Grid>
+          <Grid item xs={3} md={2}>{t.price.toFixed(4)}</Grid>
+          <Grid item xs={3} md={2}>
             <MoneyFormat amount={(t.shares * t.price).toFixed(2)} />
           </Grid>
+          <Hidden mdUp>
+            <Grid item xs={1} className={classes.accountName}></Grid>
+            <Grid item xs={11} className={classes.accountName}>{t.sourceAccountName}</Grid>
+            <Divider light />
+          </Hidden>
         </React.Fragment>
       )}
     </Grid>
