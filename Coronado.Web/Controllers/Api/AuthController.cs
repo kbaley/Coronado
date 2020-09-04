adbccf3947b9c95e983447b12f29a701f593c6b8
Kyle Baley <kyle@baley.org>
2020-09-04 15:14:28 -0500

    Add quick way to create new users (commented out)

--- Coronado.Web/Controllers/Api/AuthController.cs
+++ Coronado.Web/Controllers/Api/AuthController.cs
@@ -10,6 +10,8 @@ using System.Security.Claims;
 using System.Text;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.Extensions.Logging;
+using Coronado.Web.Domain;
+using System.Threading.Tasks;
 
 namespace Coronado.Web.Controllers.Api
 {
@@ -34,6 +36,33 @@ namespace Coronado.Web.Controllers.Api
         public string GetPassword(string password) {
             return Crypto.HashPassword(password);
         }
+
+/*
+        [HttpGet]
+        [Route("[action]")]
+        public async Task CreateUser() {
+            // Quick and dirty way to create a new user
+            // Fill in the details below and navigate to /api/auth/CreateUser
+            var email = "";
+            var name = "";
+            var password = "";
+
+            var user = new User{
+                Name = name,
+                Email = email,
+                UserId = Guid.NewGuid(),
+                Password = Crypto.HashPassword(password)
+            };
+
+            var existingUser = _context.Users.SingleOrDefault(u => u.Email == email);
+            if (existingUser != null) {
+                throw new Exception("User exists");
+            }
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
+*/
 
         [HttpPost]
         [Route("[action]")]
